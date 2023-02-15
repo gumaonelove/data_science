b2803aa8cc60389e48aeff8adb89b6d2ddea5ad0
gumaonelove <68272600+gumaonelove@users.noreply.github.com>
2023-02-15 16:27:34 +0300

    fix

--- toxic_message_detection_api/backend_service/gunicorn_conf.py
+++
@@ -1,8 +0,0 @@
-from multiprocessing import cpu_count
-
-# Socket Path
-bind = 'unix:/home/gumeonelove/chelny_working/backend/gunicorn.sock'
-
-# Worker Options
-workers = cpu_count() + 1
-worker_class = 'uvicorn.workers.UvicornWorker'
